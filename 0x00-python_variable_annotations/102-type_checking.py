d0234716ea305c655a356db80e9f0cd97c9075aa
yeretaweh <yeretaweh@gmail.com>
2024-10-10 13:36:41 +0000

    update

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
-from typing import List, Tuple, Union
+"""This module defines a type-annotated function zoom_array
+that creates a zoomed-in version of the input tuple by a given factor.
+"""
 
-def zoom_array(lst: Union[List[int], Tuple[int, ...]], factor: int = 2) -> List[int]:
-    zoomed_in: List[int] = [
-        item for item in lst
-        for i in range(factor)
-    ]
+from typing import Tuple, List, Any
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """The zoom_ray function creates a zoomed-in version of the input tuple"""
+    zoomed_in: List = [item for item in lst for i in range(factor)]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3)  # Changed 3.0 to 3 (factor should be an integer)
+zoom_3x = zoom_array(array, 3)
+
 
